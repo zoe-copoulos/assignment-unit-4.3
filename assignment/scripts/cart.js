2bc7e6a4573d1e4abc1fb398faf0c9bb6726e06a
Dane Smith <doctorhowser@gmail.com>
2023-09-05 10:03:46 -0500

    Update cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,11 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
 
-function addItem() {
-    basket.push('Kale');
-}
 
 
 
