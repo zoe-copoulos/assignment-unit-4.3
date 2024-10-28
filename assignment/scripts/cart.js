f01cf0effed3b2b39a6ce15e9d814ea5a9f9188e
Zoe Copoulos <zoe.copoulos@gmail.com>
2024-10-27 22:10:47 -0500

    good god finally no red text

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,71 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+const maxItems = 5;
+
+function addItem( item ) {
 
 
+    if (isFull()) {
+        return false;
+    }
+    else {
+        basket.push( item );
+        return true;
+    }
+ }
+
+console.log ( basket );
+
+addItem('Broccoli');
+addItem('Green Beans');
+addItem('Garlic');
+addItem('Chicken');
+addItem('Stock');
+addItem('Lemons');
+
+ function listItems() {
+    console.log( 'listItems' );
+    for ( let item of basket ){
+        console.log( item );
+    }
+ }
+
+ console.log ( basket );
+  
+ function empty() {
+    basket.length = 0;
+    console.log( basket );
+ }
+ 
+ console.log( basket );
+ 
+ function isFull() {
+
+    if ( basket.length >= maxItems ) {
+        return true;
+    }
+    else {
+
+        return false;
+
+    }
+ }
+ 
+ console.log( basket );
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
 
 
 
